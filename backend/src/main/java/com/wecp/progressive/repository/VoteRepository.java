d667881f60ed601b06a31baa58431d63cb460c6b
username <wecp-dummy@gmail.com>
2025-09-27 20:00:25 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/VoteRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/VoteRepository.java
@@ -1,5 +1,24 @@
 package com.wecp.progressive.repository;
 
+import javax.transaction.Transactional;
 
-public interface VoteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.wecp.progressive.entity.Vote;
+
+public interface VoteRepository extends JpaRepository<Vote,Integer>{
+    //@Query("select count(v) from Vote v where v.category = :category")
+    Long countByCategory(String category);
+    
+    @Modifying
+    @Transactional
+    @Query("delete from Vote v where v.team.teamId = :teamId")
+    void deleteByTeamId(int teamId);
+
+    @Modifying
+    @Transactional
+    @Query("delete from Vote v where v.cricketer.cricketerId = :cricketerId")
+    void deleteByCricketerId(int cricketerId);
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/VoteServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/VoteServiceImpl.java
@@ -1,5 +1,42 @@
 package com.wecp.progressive.service.impl;
 
-public class VoteServiceImpl  {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wecp.progressive.entity.Vote;
+import com.wecp.progressive.repository.VoteRepository;
+import com.wecp.progressive.service.VoteService;
+
+@Service
+public class VoteServiceImpl implements VoteService  {
+    
+    @Autowired
+    private VoteRepository voteRepository;
+
+    public List<Vote> getAllVotes(){
+        return voteRepository.findAll();
+    }
+    public int createVote(Vote vote){
+        voteRepository.save(vote);
+        return vote.getVoteId();
+    }
+    public Map<String,Long> getVotesCountOfAllCategories(){
+        Map<String,Long> map = new HashMap<>();
+
+        map.put("Team", voteRepository.countByCategory("Team"));
+        map.put("Batsman",voteRepository.countByCategory("Batsman"));
+        map.put("Bowler",voteRepository.countByCategory("Bowler"));
+        map.put("All-rounder",voteRepository.countByCategory("All-rounder"));
+        map.put("Wicketkeeper",voteRepository.countByCategory("Wicketkeeper"));
+
+        return map;
+        
+        
+    }
 }
