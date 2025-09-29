117c02bb364772dd65a81dae27a0dd77558a9f7b
username <wecp-dummy@gmail.com>
2025-09-29 13:34:11 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/MatchRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/MatchRepository.java
@@ -1,5 +1,28 @@
 package com.wecp.progressive.repository;
 
+import java.util.List;
 
-public interface MatchRepository {
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.wecp.progressive.entity.Match;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Integer>
+{
+    public Match findByMatchId(int matchId);
+    public List<Match> findAllByStatus(String status);
+    @Modifying
+    @Transactional
+    @Query("Delete from Match m WHERE m.firstTeamId = :teamId OR m.secondTeamId = :teamId")
+    public void deleteByTeamId(@Param("teamId") int teamId);
+    
+
+    
+
 }
