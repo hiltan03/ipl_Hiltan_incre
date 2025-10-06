547df3a1a729bbabe1ff0cff2b8723bca1bedf65
Krishna Prasath S <85412620+Krishnaprasath1@users.noreply.github.com>
2025-10-06 21:59:12 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/wecp/progressive/config/SecurityConfig.java
+++ backend/src/main/java/com/wecp/progressive/config/SecurityConfig.java
@@ -10,63 +10,61 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.wecp.progressive.jwt.JwtRequestFilter;
+import com.wecp.progressive.service.impl.UserLoginServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
+public class SecurityConfig extends WebSecurityConfigurerAdapter 
+{
+    private UserLoginServiceImpl userLoginServiceImpl;
     private JwtRequestFilter jwtRequestFilter;
+    private PasswordEncoder passwordEncoder;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    @Autowired
+    public SecurityConfig(UserLoginServiceImpl userLoginServiceImpl, JwtRequestFilter jwtRequestFilter,
+            PasswordEncoder passwordEncoder) {
+        this.userLoginServiceImpl = userLoginServiceImpl;
+        this.jwtRequestFilter = jwtRequestFilter;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.userDetailsService(userLoginServiceImpl).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .cors()
-            .and()
-            .csrf().disable()
-            .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
-
-                .antMatchers("/attendance/**").hasAuthority("TEACHER")
-                .antMatchers(HttpMethod.GET, "/attendance/**").hasAuthority("STUDENT")
-
-                .antMatchers("/course/**").hasAuthority("TEACHER")
-                .antMatchers(HttpMethod.GET, "/course/**").hasAuthority("STUDENT")
-
-                .antMatchers("/enrollment/**").hasAuthority("TEACHER")
-                .antMatchers(HttpMethod.GET, "/enrollment/**").hasAuthority("STUDENT")
-
-                .antMatchers("/student/**").hasAuthority("STUDENT")
-                .antMatchers("/teacher/**").hasAuthority("TEACHER")
-
-                .anyRequest().authenticated();
+        http.cors().and()
+                .authorizeRequests()
+                //.anyRequest().permitAll() // Allow all requests without authentication
+                .antMatchers(HttpMethod.POST, "/user/register", "/user/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/team", "/team/**", "/cricketer", "/cricketer/**", "/match", "/match**").hasAuthority("USER")
+                .antMatchers(HttpMethod.POST, "/vote", "/ticket").hasAuthority("USER")
+                .antMatchers(HttpMethod.POST, "/vote", "/team/**", "/cricketer/", "/match").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/team**", "/cricketer/**", "/match/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/team/**", "/cricketer/**", "/match/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/vote/**", "/ticket/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Override
+
     @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
+    public AuthenticationManager authenticationManagerBean() throws Exception
+    {
         return super.authenticationManagerBean();
     }
 
-    
-
-    
 }
